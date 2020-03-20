6b2fd4f9563fd596be239cb74bb233774c62cadc
Sergey Ivzhenko <sergey.ivzhenko@eosda.com>
2020-03-20 20:02:55 +0200

    add accessors reminder

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,16 @@
 import {User} from "./models/User";
 
 const user = new User({name: 'new record', age: 0});
-user.events.on('change', () => {
-    console.log('change');
-});
-user.events.trigger('change');
+
+// A quick reminder on accessors
+
+class Person {
+    constructor(public firstName: string, public lastName: string) {}
+
+    get fullName(): string {
+        return `${this.firstName} ${this.lastName}`;
+    }
+}
+
+const person = new Person('firstname', 'lastname');
+console.log(person.fullName);
