2fd40f047f678fb70eb7ede760e246f13293e341
Sergey Ivzhenko <sergey.ivzhenko@eosda.com>
2020-03-21 12:42:00 +0200

    implement set accessor

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,4 @@ import {User} from "./models/User";
 const user = new User({name: 'new record', age: 0});
 console.log(user.get('name'));
 user.on('change', () => console.log('user changed'));
-user.trigger('change');
+user.set({name: 'new name'});
