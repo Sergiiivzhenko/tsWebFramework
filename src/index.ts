2d335f7ed402a75346e50e1169b992b1241f3216
Sergey Ivzhenko <sergey.ivzhenko@eosda.com>
2020-03-21 12:38:50 +0200

    setup 3 simple accessors, fixed context issue with normal functions

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,6 @@
 import {User} from "./models/User";
 
 const user = new User({name: 'new record', age: 0});
-
-// A quick reminder on accessors
-
-class Person {
-    constructor(public firstName: string, public lastName: string) {}
-
-    get fullName(): string {
-        return `${this.firstName} ${this.lastName}`;
-    }
-}
-
-const person = new Person('firstname', 'lastname');
-console.log(person.fullName);
+console.log(user.get('name'));
+user.on('change', () => console.log('user changed'));
+user.trigger('change');
