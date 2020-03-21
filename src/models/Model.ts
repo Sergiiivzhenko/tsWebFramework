f2f610bf95f9211f652b2ded6ff2e7596c676486
Sergey Ivzhenko <sergey.ivzhenko@eosda.com>
2020-03-21 13:19:36 +0200

    adding interfaces for model class

---
+++ src/models/Model.ts
@@ -0,0 +1,21 @@
+import {AxiosPromise} from "axios";
+
+interface ModelAttributes<T> {
+    set: (value: T) => void;
+    get: <K extends keyof T>(propName: K) => T[K];
+    getAll: () => T;
+}
+
+interface Sync<T> {
+    fetch: (id: number) => AxiosPromise;
+    save: (data: T) => AxiosPromise;
+}
+
+interface Events {
+    on: (eventName: string, callback: () => void) => void;
+    trigger: (eventName: string) => void;
+}
+
+export class Model {
+
+}
