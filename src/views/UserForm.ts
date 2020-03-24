1bc1e2294a3870661293a3a8fd5872e1b60eaa51
Sergey Ivzhenko <sergey.ivzhenko@eosda.com>
2020-03-24 07:28:41 +0200

    save user added to form

--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -6,14 +6,15 @@ export class UserForm extends View<User, UserProps> {
         return {
             'click:.set-age': this.onSetAgeClick,
             'click:.set-name': this.onSetNameClick,
+            'click:.save-model': this.onSaveClick,
         };
     };
 
-    onSetAgeClick = () => {
+    onSetAgeClick = (): void => {
         this.model.setRandomAge();
     };
 
-    onSetNameClick = () => {
+    onSetNameClick = (): void => {
         const input = document.querySelector('input');
         if (input) {
             const name = input.value;
@@ -21,15 +22,18 @@ export class UserForm extends View<User, UserProps> {
         }
     };
 
+    onSaveClick = (): void => {
+        this.model.save();
+    };
+
     template = (): string => {
         return `
             <div>
                 <h1>User Form</h1>
-                <div>User name: ${this.model.get('name')}</div>
-                <div>User age: ${this.model.get('age')}</div>
-                <input />
+                <input placeholder="${this.model.get('name')}" />
                 <button class="set-name">Set Name</button>
                 <button class="set-age">Set random age</button>
+                <button class="save-model">Save user</button>
             <div>
         `;
     };
