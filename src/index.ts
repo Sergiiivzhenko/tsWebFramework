48bf3cbac9eec9ef279ef1406289995247483ae6
Sergey Ivzhenko <sergey.ivzhenko@eosda.com>
2020-03-24 08:40:57 +0200

    added collection displaying

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,15 @@
-import {User} from "./models/User";
-import {UserEdit} from "./views/UserEdit";
+import {rootUrl, User, UserProps} from "./models/User";
+import {Collection} from "./models/Collection";
+import {UserList} from "./views/UserList";
 
-const user = User.buildUser({name: 'NAME', age: 20});
-const root = document.getElementById('root');
-if (root) {
-    const userEdit = new UserEdit(root, user);
-    userEdit.render();
-    console.log(userEdit);
-} else {
-    throw new Error('Root element not found');
-}
+const users = new Collection(rootUrl, (json: UserProps) => {
+    return User.buildUser(json);
+});
+users.on('change', () => {
+    const root = document.getElementById('root');
+    console.log(root, users);
+    if (root) {
+        new UserList(root, users).render();
+    }
+});
+users.fetch();
